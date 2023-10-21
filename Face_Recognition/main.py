4c4599b7d2ce39d5be8f4c68f35b21779c9f0e8f
jazzpher <80498982+jazzpher@users.noreply.github.com>
2023-10-21 12:01:32 +0800

    Update main.py
    - Error handling to check if the camera and cascade classifiers are successfully opened and loaded.
    - Proper handling of reading frames from the camera.
    - Added comments for better code readability and understanding.
    - Enhanced parameters for face detection using the `scaleFactor` and `minNeighbors` settings.
    - Properly releasing the camera and closing OpenCV windows when the program exits.

--- Face_Recognition/main.py
+++ Face_Recognition/main.py
@@ -1,25 +1,47 @@
 import cv2
 
+# Open the default camera (index 0)
 cap = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-body_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_fullbody.xml")
+# Check if the camera is successfully opened
+if not cap.isOpened():
+    print("Error: Camera not found or cannot be opened.")
+    exit()
+
+# Load the Haar Cascade classifiers for face and body detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
+# Check if the cascade classifiers are loaded successfully
+if face_cascade.empty() or body_cascade.empty():
+    print("Error: Haar Cascade files not loaded.")
+    exit()
 
 while True:
-    _, frame = cap.read()
+    # Read a frame from the camera
+    ret, frame = cap.read()
 
+    if not ret:
+        print("Error: Cannot read a frame from the camera.")
+        break
+
+    # Convert the frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    # Draw rectangles around detected faces
     for (x, y, width, height) in faces:
-       cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
 
+    # Display the frame in a window named "Camera"
     cv2.imshow("Camera", frame)
 
+    # Check for the 'q' key press to exit the loop
     if cv2.waitKey(1) == ord('q'):
         break
 
+# Release the camera and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
